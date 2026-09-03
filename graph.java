b83982a26618baa944571bf2e8181c8b1d82745d
Ravi singh <ravisingh98199@gmail.com>
2026-09-04 00:22:58 +0530

    Add Prim's algorithm for Minimum Spanning Tree
    Implemented Prim's algorithm to find the Minimum Spanning Tree (MST) of a graph.

--- graph.java
+++ graph.java
@@ -859,3 +859,68 @@ public class graph {
         allPath(graph, src, dest, "");
     }
 }
+
+                                  /*Minimum Spanning Tree (MST)
+A minimum spanning tree (MST) or minimum weight spanning tree is a subset of the
+edges of a connected, edge-weighted undirected graph that connects all the vertices
+together, without any cycles and with the minimum possible total edge weight. */
+
+// prims algorithm./////////////////////
+import java.util.*;
+public class graph {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 12));
+        graph[1].add(new Edge(1, 2, 5));
+
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 1, 5));
+        graph[2].add(new Edge(2, 3, 10));
+
+        graph[3].add(new Edge(3, 1, 12));
+        graph[3].add(new Edge(3, 2, 10));
+    }
+    public static void prims(ArrayList<Edge> graph[], int V) {
+        boolean vis[] = new boolean[V];
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.wt - b.wt);
+        pq.add(new Edge(0, 0, 0)); 
+
+        while (!pq.isEmpty()) {
+            Edge curr = pq.poll();
+            if (!vis[curr.dest]) {
+                vis[curr.dest] = true;
+                System.out.println("Edge: " + curr.src + " - " + curr.dest + " Weight: " + curr.wt);
+
+                for (Edge e : graph[curr.dest]) {
+                    if (!vis[e.dest]) {
+                        pq.add(e);
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int V = 4;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        prims(graph, V);
+    }
+}
